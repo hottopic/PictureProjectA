13e9f5525730ba7b0c30d3520af8bf3d9dbb4dd8
William Yeung <william@williams-macbook-air.local>
2018-08-12 14:50:45 +1200

    Initial Commit

---
+++ PictureProjectA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PictureProjectA
+//
+//  Created by William Yeung on 12/08/18.
+//  Copyright © 2018 William Yeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
