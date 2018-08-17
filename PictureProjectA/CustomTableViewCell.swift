40841f26d926e0b4de26bef70e9a15ad52e89e60
William Yeung <william@williams-macbook-air.local>
2018-08-17 19:42:12 +1200

    .

---
+++ PictureProjectA/CustomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTableViewCell.swift
+//  PictureProjectA
+//
+//  Created by William Yeung on 16/08/18.
+//  Copyright © 2018 William Yeung. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
